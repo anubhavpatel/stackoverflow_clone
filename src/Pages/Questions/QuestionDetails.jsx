7f8c72b0e021893231610fb0ac17c2a7b7fdaacb
anubhavpatel <anubhavpatel6391@gmail.com>
2022-08-09 16:52:37 +0530

    addad

--- src/Pages/Questions/QuestionDetails.jsx
+++ src/Pages/Questions/QuestionDetails.jsx
@@ -100,7 +100,7 @@ const QuestionDetails = () => {
                      <div className="question-action-user">
                         <div>
                             <button type='button'>Share</button>
-                            <button type='button'>Delete</button>
+                            <button type='button'>Delete kr betichod</button>
                         </div>
                         <div>
                             <p>asked by {question.time}</p>
